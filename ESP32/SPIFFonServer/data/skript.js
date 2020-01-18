8d8e3676f1dd10f7caed5a6a066ea90fd9f4fa4c
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2020-01-18 18:15:23 +0800

    Removed console log statements

--- ESP32/SPIFFonServer/data/skript.js
+++ ESP32/SPIFFonServer/data/skript.js
@@ -1,7 +1,5 @@
 button = document.getElementById('datebt').addEventListener('click', dateButton)
-console.log('hiii')
 
 function dateButton() {
-	console.log('hai')
 	document.getElementById('demo').innerHTML = Date()
 }
