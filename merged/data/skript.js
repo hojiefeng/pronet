9a520b129a06e3aa2c3b1c9415b80c03f920cc8b
Ho Jie Feng <hjf3108@gmail.com>
2020-01-18 20:59:38 +0800

    merge new html code

--- merged/data/skript.js
+++ merged/data/skript.js
@@ -1,5 +1,18 @@
-button = document.getElementById('datebt').addEventListener('click', dateButton)
+const marea = document.getElementById('marea')
 
-function dateButton() {
-	document.getElementById('demo').innerHTML = Date()
+const msgRecieved = (msg) => { marea.innerHTML = '[' + Date.now() + '] ' + msg + '<br>' + marea.innerHTML }
+
+/* 
+
+//Connect to websocket
+let exampleSocket = new WebSocket("ws://192.168.4.1:81/")
+
+// Sending
+exampleSocket.send("Here's some text that the server is urgently awaiting!")
+
+// Receiving
+exampleSocket.onmessage = function (event) {
+	console.log(event.data);
 }
+*/
+msgRecieved('Waiting for messages...')
